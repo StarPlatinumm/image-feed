e34cb6acd19488993900001ddd0bca12eef31118
Кривдин Артем <akrivdin@data-platform.ru>
2024-05-18 21:46:07 +0300

    Добавил экран с профилем юзера

---
+++ image-feed/Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userId: UILabel!
+    @IBOutlet weak var userText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
