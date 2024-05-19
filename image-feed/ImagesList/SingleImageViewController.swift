09afd18bd59088a3625ba52c744788b676ae58a6
Кривдин Артем <akrivdin@data-platform.ru>
2024-05-19 20:39:55 +0300

    Заменил Navigation Controller на сегвей с "Present Modally"

--- image-feed/ImagesList/SingleImageViewController.swift
+++ image-feed/ImagesList/SingleImageViewController.swift
@@ -10,6 +10,10 @@ final class SingleImageViewController: UIViewController {
     
     @IBOutlet private weak var imageView: UIImageView!
     
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
