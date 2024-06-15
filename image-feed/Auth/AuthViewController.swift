62de85c8c4b96797e728944772509d5ea36595af
Кривдин Артем <akrivdin@data-platform.ru>
2024-06-15 17:58:19 +0300

    Добавил индикатор загрузки (ProgressHUD)

--- image-feed/Auth/AuthViewController.swift
+++ image-feed/Auth/AuthViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import ProgressHUD
 
 protocol AuthViewControllerDelegate: AnyObject {
     func didAuthenticate(_ vc: AuthViewController)
@@ -39,8 +40,12 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        ProgressHUD.animate() // показать индикатор загрузки
+        
         oauth2Service.fetchOAuthToken(code: code) { [weak self] result in
             guard let self = self else { return }
+            
+            ProgressHUD.dismiss() // скрыть индикатор загрузки
             
             switch result {
             case .success:
